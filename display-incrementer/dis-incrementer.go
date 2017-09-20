172b7332d3357f30b9402442230a59c65d6f0f54
Colin Nelson <orthros@users.noreply.github.com>
2017-09-20 12:01:40 -0500

    Updated the display incrementer to be more go like Now using a few goroutines and isolating the core logic better Still have the gpio calls commented out because they are raspberry pi specific

--- display-incrementer/dis-incrementer.go
+++ display-incrementer/dis-incrementer.go
@@ -14,13 +14,6 @@ func main() {
 	redisChannelName := os.Getenv("UNSPEAK_CHANNEL_SPOKEN")
 	redisAddress := os.Getenv("UNSPEAK_REDIS_ADDRESS")
 
-	// err := rpio.Open(); err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-
-	// defer rpio.Close();
-
 	client := redis.NewClient(&redis.Options{
 		Addr:     redisAddress,
 		Password: "", // no password set
@@ -28,22 +21,50 @@ func main() {
 	})
 	defer client.Close()
 
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
-
 	pubsub := client.Subscribe(redisChannelName)
 	defer pubsub.Close()
 
-	for {
-		msg, err := pubsub.ReceiveMessage()
-		if err != nil {
-			panic(err)
-		}
-		//At this point we should increment the counter
-		//pin = rpio.Pin(10);
-		//pin.Toggle();
-		fmt.Println(msg)
-	}
+	done := make(chan bool)
+	msg := make(chan string)
+
+	go pinWorker(msg)
+	go redisWorker(pubsub, msg, done)
+
+	//Wait until we are Done...
+	<-done
 
 	fmt.Println("Exiting")
+}
+
+func redisWorker(pubsub *redis.PubSub, msg chan<- string, done chan<- bool) {
+	for {
+		message, err := pubsub.ReceiveMessage()
+		if err != nil {
+			break
+		}
+		msg <- message.Payload
+	}
+	close(msg)
+	done <- true
+}
+
+func pinWorker(msg <-chan string) {
+
+	// err := rpio.Open(); err != nil {
+	// 	fmt.Println(err)
+	// 	os.Exit(1)
+	// }
+
+	// defer rpio.Close();
+
+	//pin := rpio.Pin(10);
+	for {
+		message, more := <-msg
+		if !more {
+			break
+		}
+		//At this point we should increment the counter
+		//pin.Toggle();
+		fmt.Println(message)
+	}
 }

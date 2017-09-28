49e6a7b223bba01a14b3631ad50a391b72c6d864
Colin Nelson <orthros@users.noreply.github.com>
2017-09-28 12:11:14 -0500

    Updated the sonic-listener. Abandoning pocketsphinx as test showed it was less than stellar at word recognition without significant training. Now opting to use Wit.ai. Also Added basic scaffolding for the pubsub & prhase recognition in the listener. Needs to be tested as I have neither a microphone nor Redis on this box

--- sonic-listener/sonic-listener.py
+++ sonic-listener/sonic-listener.py
@@ -4,16 +4,48 @@
 
 import speech_recognition as sr
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+import time
 
-# recognize speech using Sphinx
-try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
-except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+#redis-py
+import redis 
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path, override=True)
+
+REDIS_SERVER = os.environ.get('REDIS_SERVER')
+REDIS_PORT = os.environ.get('REDIS_PORT')
+REDIS_PUBLISH_CHANNEL = os.environ.get('REDIS_PUBLISH_CHANNEL')
+REDIS_COLLECTION_NAME = os.environ.get('REDIS_COLLECTION_NAME')
+
+r = redis.StrictRedis(host=REDIS_SERVER, port=REDIS_PORT,db=0)
+
+pubsub = r.pubsub()
+phrases = r.smembers(REDIS_COLLECTION_NAME)
+
+WIT_API_KEY = os.environ.get('WIT_API_KEY')
+
+def callback(recognizer, audio):
+    try:
+        print('Wit.ai things you said ' + recognizer.recognize_wit(audio, WIT_API_KEY))
+        # Do some work to see if the words are in the set
+        # Foreach phrase in phrases, foreach word in witWords if regex is match phrase word publish 
+        pubsub.publish(REDIS_PUBLISH_CHANNEL, "the word that got matched")
+    except sr.UnknownValueError:
+        print('Wit.ai could not understand audio')
+        pass
+    except sr.RequestError as e:
+        print('Could not request results from Wit.ai service; {0}'.format(e))
+
+r = sr.Recognizer()
+m = sr.Microphone()
+with m as source:
+    r.adjust_for_ambient_noise(source)
+
+stop_listening = r.listen_in_background(m,callback)
+
+while True : time.sleep(0.1)
+
+stop_listening()

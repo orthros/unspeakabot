922dd78cd655a89d72a22c11be8c32fea8551f8e
Colin <colinnlsn@gmail.com>
2017-10-01 17:00:03 -0500

    Revert "Fixed deprication warning" I misread the documentation. This requires more changes than just a capitalization. Undoing until I have more time to research This reverts commit 50283b0b64f4b3a9f45f790fe7ab28be75eebb30.

--- web-client/app/routes.js
+++ web-client/app/routes.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
     // frontend routes =========================================================
     // route to handle all angular requests
     app.get('*', function (req, res) {
-        res.sendFile('./public/index.html');
+        res.sendfile('./public/index.html');
     });
 
 };

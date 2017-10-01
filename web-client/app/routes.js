50283b0b64f4b3a9f45f790fe7ab28be75eebb30
Colin <colinnlsn@gmail.com>
2017-10-01 16:54:25 -0500

    Fixed deprication warning `sendfile` => `sendFile` for express

--- web-client/app/routes.js
+++ web-client/app/routes.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
     // frontend routes =========================================================
     // route to handle all angular requests
     app.get('*', function (req, res) {
-        res.sendfile('./public/index.html');
+        res.sendFile('./public/index.html');
     });
 
 };
